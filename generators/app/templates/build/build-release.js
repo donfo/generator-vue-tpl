eebd9c6038a56f58bf1f5b7b297ef3ce3a1e93fa
wangmeng <wangnew2013@126.com>
2017-07-24 17:12:55 +0800

    0.2.3 bug fix

--- generators/app/templates/build/build-release.js
+++ generators/app/templates/build/build-release.js
@@ -1,10 +1,6 @@
 require('./check-versions')()
 
-var config = require('../config')
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
-}
-console.log(`process.env.NODE_ENV=${process.env.NODE_ENV} in env`)
+process.env.NODE_ENV = 'releasing'
 
 var fs = require('fs')
 var ora = require('ora')
